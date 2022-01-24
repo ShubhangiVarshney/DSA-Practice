f5ebda5c98942a2213cffe5c63bc6dcc93b2f352
Shubhangi Varshney <varshne1@uwindsor.ca>
2022-01-24 00:53:46 -0500

    Binary search- not working for few elements again

--- DSA/src/BinarySearchAlgo.java
+++ DSA/src/BinarySearchAlgo.java
@@ -2,7 +2,7 @@ public class BinarySearchAlgo {
     public boolean BinarySearch(int n, int[] arr){
         int length=arr.length;
         int left=0, right=length-1;
-        while(left<right) {
+        while(left<=right) {
             int middleIndex = ((left + right) / 2) +1;
             int number = arr[middleIndex];
             if(number==n)
@@ -20,7 +20,7 @@ public class BinarySearchAlgo {
 
     public static void main (String[] args){
         int[] array={1,23,45,67,78,79,84,89,101,156,187,276,333,390,401,455,491,506,528,735,936};
-        int n=528;
+        int n=1;
         BinarySearchAlgo bs=new BinarySearchAlgo();
         boolean ans=bs.BinarySearch(n,array);
         System.out.println(ans);

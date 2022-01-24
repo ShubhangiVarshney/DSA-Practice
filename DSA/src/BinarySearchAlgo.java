12a2da11a45a89f6158b2745851943ffcb292f54
Shubhangi Varshney <varshne1@uwindsor.ca>
2022-01-24 00:39:52 -0500

    Binary search- not working for first and last element

--- DSA/src/BinarySearchAlgo.java
+++ DSA/src/BinarySearchAlgo.java
@@ -2,22 +2,25 @@ public class BinarySearchAlgo {
     public boolean BinarySearch(int n, int[] arr){
         int length=arr.length;
         int left=0, right=length-1;
-        while(left<right) {
-            int middleIndex = (left + right) / 2;
+        while(left<right-1) {
+            int middleIndex = ((left + right) / 2) +1;
             int number = arr[middleIndex];
             if(number==n)
                 return true;
-            else if(n<number)
+            else if(n<number){
                 right=middleIndex;
-                else
+                System.out.println("right="+right);}
+                else {
                  left=middleIndex;
+                System.out.println("left="+left);}
+
             }
         return false;
     }
 
     public static void main (String[] args){
         int[] array={1,23,45,67,78,79,84,89,101,156,187,276,333,390,401,455,491,506,528,735,936};
-        int n=77;
+        int n=1;
         BinarySearchAlgo bs=new BinarySearchAlgo();
         boolean ans=bs.BinarySearch(n,array);
         System.out.println(ans);

d36cd3c0e8372821e6f5bac054e3ab4ca3c891e8
Shubhangi Varshney <varshne1@uwindsor.ca>
2022-02-03 17:26:50 -0500

    Q112 unsolved

---
+++ DSA/src/PracticeQuestions/Q112PathSum.java
@@ -0,0 +1,30 @@
+package PracticeQuestions;
+
+import java.util.Stack;
+
+public class Q112PathSum {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root==null)
+            return false;
+
+        int sum=0;
+        sum=sum+root.val;
+        return sumPath(root.left, targetSum, sum)
+            }
+
+            public boolean sumPath(TreeNode root, int targetSum, int sum){
+
+            }
+}

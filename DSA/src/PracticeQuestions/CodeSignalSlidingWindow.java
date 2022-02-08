93d4cbf600d45957753fef044ef86d938dd5a38a
Shubhangi Varshney <varshne1@uwindsor.ca>
2022-02-07 20:43:45 -0500

    CodeSignal question update

--- DSA/src/PracticeQuestions/CodeSignalSlidingWindow.java
+++ DSA/src/PracticeQuestions/CodeSignalSlidingWindow.java
@@ -2,8 +2,8 @@ package PracticeQuestions;
 
 public class CodeSignalSlidingWindow {
     public static void SlideWindow(int[][] arr){
-        for (int i=0; i<arr.length; i++){
-            for (int j=0; j<arr[i].length; j++){
+        for (int i=1; i<arr.length; i++){
+            for (int j=1; j<arr[i].length; j++){
                 System.out.print(" "+arr[i][j]);
             }
             System.out.println();
